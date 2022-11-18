649073582e9036b765551f23179995cb22b3d957
rizqiindah03 <rizqiindah32@gmail.com>
2022-11-18 17:45:30 +0700

    membuat method listempty

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -107,5 +107,13 @@ namespace double_linked_list
             current.next.prev = previous;
             return true;
         }
+        public bool listEmpty()
+        {
+            if (START == null)
+                return true;
+            else
+                return false;
+        }
+       
     }
 }
