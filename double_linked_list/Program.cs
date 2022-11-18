ca61f627edea5230063458b336ac66afd9ee2516
rizqiindah03 <rizqiindah32@gmail.com>
2022-11-18 20:14:42 +0700

    membuat case 4

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -181,6 +181,11 @@ namespace double_linked_list
                                     Console.WriteLine("Record with roll number " + rollNo + "deleted \n");
                             }
                             break;
+                        case '3':
+                            {
+                                obj.ascending();
+                            }
+                            break;
                     }
                 }
             }
